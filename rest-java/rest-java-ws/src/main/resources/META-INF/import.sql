1c3317fdfedc0cd7f0cd46ae3597fbe15608eb77
Renato Herebia <rherebia@gmail.com>
2017-04-22 13:51:57 -0400

    Configurando os scripts para criar e remover o banco com Derby.

--- rest-java/rest-java-ws/src/main/resources/META-INF/import.sql
+++ rest-java/rest-java-ws/src/main/resources/META-INF/import.sql
@@ -1,1 +1,5 @@
 insert into teste.usuario (id, nome) values (-1, 'Renato Herebia');
+insert into teste.usuario (id, nome) values (-2, 'Jose');
+insert into teste.usuario (id, nome) values (-3, 'Joao');
+insert into teste.usuario (id, nome) values (-4, 'Marcos');
+insert into teste.usuario (id, nome) values (-5, 'Maria');
